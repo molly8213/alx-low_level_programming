3e0871190a3d49086752c471a59994e5a7372b4b
molly8123 <tshonuyi43@gmail.com>
2022-12-08 06:59:47 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
-
 /**
+ * main - prints exactly and that piece of art is useful
  *
- *  * main - prints exactly and that piece of art is useful
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
